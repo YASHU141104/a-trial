ad227c3616b220b18274b863555298fb732643d6
YASHU141104 <127117031@sastra.ac.in>
2025-10-20 17:34:22 +0530

    Update news-category.js

--- news-category.js
+++ news-category.js
@@ -1,8 +1,8 @@
 // ===== SETUP SUPABASE CLIENT =====
 // Replace with your actual Supabase details:
 const supabase = supabase.createClient(
-  'https://xddssiompemprjbnxxlf.supabase.co',  
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkZHNzaW9tcGVtcHJqYm54eGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMDczNzcsImV4cCI6MjA3Mzc4MzM3N30.QHjF8xdFYp6ex1YW2XV6GkKvPZXNp1biImoQIZdSMG4'               
+const supabase_url = "https://xddssiompemprjbnxxlf.supabase.co";
+const supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkZHNzaW9tcGVtcHJqYm54eGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyMDczNzcsImV4cCI6MjA3Mzc4MzM3N30.QHjF8xdFYp6ex1YW2XV6GkKvPZXNp1biImoQIZdSMG4";
 );
 
 // ===== GET CATEGORY FROM URL =====
